7c38866b7382467c203fa5894fd6fc2a349d0bc5
pasqualespica <36746022+pasqualespica@users.noreply.github.com>
2022-02-18 15:54:47 +0100

    add nodo mock

--- reporting-batch/src/main/java/it/gov/pagopa/reporting/RetrieveFlows.java
+++ reporting-batch/src/main/java/it/gov/pagopa/reporting/RetrieveFlows.java
@@ -3,7 +3,19 @@ package it.gov.pagopa.reporting;
 import com.microsoft.azure.functions.ExecutionContext;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.QueueTrigger;
+import it.gov.pagopa.reporting.models.Organizations;
+import it.gov.pagopa.reporting.service.FlowsService;
+import it.gov.pagopa.reporting.service.NodoChiediElencoFlussi;
+import it.gov.pagopa.reporting.servicewsdl.NodoChiediElencoFlussiRendicontazione;
+import it.gov.pagopa.reporting.servicewsdl.NodoChiediElencoFlussiRendicontazioneRisposta;
+import org.jboss.resteasy.client.jaxrs.ResteasyClient;
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
 
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.xml.ws.ResponseWrapper;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -20,13 +32,25 @@ public class RetrieveFlows {
             @QueueTrigger(name = "RetrieveOrganizationsTrigger", queueName = "%ORGANIZATIONS_QUEUE%", connection = "FLOW_SA_CONNECTION_STRING") String message,
             final ExecutionContext context) {
 
+        NodoChiediElencoFlussiRendicontazione req = new NodoChiediElencoFlussiRendicontazione();
+
         Logger logger = context.getLogger();
 
         try {
 
             logger.log(Level.INFO, () -> "[RetrieveOrganizationsTrigger START]  processed a message " + message);
 
-            logger.log(Level.INFO, () -> "[RetrieveOrganizationsTrigger END]  processed a message " + message);
+            //Response response = ClientBuilder.newClient()
+            Response response = new ResteasyClientBuilder().build()
+              .target("http://localhost:8086/nodo-per-pa/v1"+"/nodoChiediElencoFlussiRendicontazione")
+              .request()
+              .property("SOAPAction", "nodoChiediElencoFlussiRendicontazione")
+             .post(Entity.entity(req, MediaType.APPLICATION_XML));
+
+            logger.log(Level.INFO, () -> "[RetrieveOrganizationsTrigger END]  processed a message " + message );
+            NodoChiediElencoFlussiRendicontazioneRisposta res = response.readEntity(NodoChiediElencoFlussiRendicontazioneRisposta.class);
+            logger.log(Level.INFO, () -> "[RetrieveOrganizationsTrigger END]  response " + res);
+
         } catch (Exception e) {
 
             logger.log(Level.SEVERE, () -> "[RetrieveOrganizationsTrigger Error] Generic Error " + e.getMessage() + " "
@@ -35,4 +59,7 @@ public class RetrieveFlows {
 
     }
 
+  public NodoChiediElencoFlussi getNodeClientInstance() {
+    return new NodoChiediElencoFlussi();
+  }
 }
