1e60411005657e019856bfae3f936a0f6939edd0
aacitelli <aacitelli@host.docker.internal>
2022-02-21 17:33:43 +0100

    [PPD-77] health check: implementazione DRAFT

--- gpd/src/main/java/it/gov/pagopa/debtposition/controller/BaseController.java
+++ gpd/src/main/java/it/gov/pagopa/debtposition/controller/BaseController.java
@@ -1,15 +1,65 @@
 package it.gov.pagopa.debtposition.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import it.gov.pagopa.debtposition.model.AppInfo;
+import it.gov.pagopa.debtposition.model.ProblemJson;
 
 @RestController()
 public class BaseController {
-  @GetMapping(value = "/")
-  @ResponseStatus(HttpStatus.OK)
-  public void heartBeat() {
-    // Used just for heartbeating
-  }
+    @Value("${application.name}")
+    private String name;
+
+    @Value("${application.version}")
+    private String version;
+
+    @Value("${properties.environment}")
+    private String environment;
+
+
+    /**
+     * @return redirect to Swagger page documentation
+     */
+    @Hidden
+    @GetMapping("")
+    public RedirectView home() {
+        return new RedirectView("/swagger-ui.html");
+    }
+
+    /**
+     * Health Check
+     *
+     * @return ok
+     */
+    @Operation(summary = "Return OK if application is started", security = {@SecurityRequirement(name = "ApiKey"), @SecurityRequirement(name = "Authorization")}, tags = {"Home"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK.", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = AppInfo.class))),
+            @ApiResponse(responseCode = "400", description = "Bad Request.", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ProblemJson.class))),
+            @ApiResponse(responseCode = "401", description = "Wrong or missing function key.", content = @Content(schema = @Schema())),
+            @ApiResponse(responseCode = "403", description = "Forbidden.", content = @Content(schema = @Schema())),
+            @ApiResponse(responseCode = "429", description = "Too many requests.", content = @Content(schema = @Schema())),
+            @ApiResponse(responseCode = "500", description = "Service unavailable.", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ProblemJson.class)))})
+
+    @GetMapping("/info")
+    public ResponseEntity<AppInfo> healthCheck() {
+        AppInfo info = AppInfo.builder()
+                .name(name)
+                .version(version)
+                .environment(environment)
+                .build();
+        return ResponseEntity.status(HttpStatus.OK).body(info);
+    }
 }
