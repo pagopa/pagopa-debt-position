cb8bd42031bf950443ced687a8ec5972760b3fbc
aacitelli <aacitelli@host.docker.internal>
2022-02-23 18:25:16 +0100

    [PPD-70] EC list: modifiche e aggiunta di un junit test

---
+++ gpd/src/main/java/it/gov/pagopa/debtposition/model/payments/OrganizationModelQueryBean.java
@@ -0,0 +1,23 @@
+package it.gov.pagopa.debtposition.model.payments;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrganizationModelQueryBean implements Serializable {
+ 
+	/**
+     * generated serialVersionUID
+     */
+	
+	private static final long serialVersionUID = -2167998567826059075L;
+	
+	
+    private String organizationFiscalCode;
+    
+}
