00054a8b0357cf15f6be2b1c7a91c48f45e9af40
aacitelli <aacitelli@host.docker.internal>
2022-02-23 18:25:16 +0100

    [PPD-70] EC list: modifiche e aggiunta di un junit test

--- gpd/src/main/java/it/gov/pagopa/debtposition/controller/payments/api/IPaymentsController.java
+++ gpd/src/main/java/it/gov/pagopa/debtposition/controller/payments/api/IPaymentsController.java
@@ -1,7 +1,10 @@
 package it.gov.pagopa.debtposition.controller.payments.api;
 
+import java.time.LocalDate;
+
 import javax.validation.Valid;
 
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -20,6 +24,7 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import it.gov.pagopa.debtposition.model.ProblemJson;
 import it.gov.pagopa.debtposition.model.payments.PaymentOptionModel;
+import it.gov.pagopa.debtposition.model.payments.response.OrganizationListModelResponse;
 import it.gov.pagopa.debtposition.model.payments.response.PaymentOptionModelResponse;
 import it.gov.pagopa.debtposition.model.payments.response.TransferModelResponse;
 
@@ -78,5 +83,16 @@ public interface IPaymentsController {
     		@PathVariable("iuv") String iuv, 
     		@Parameter(description = "Transaction identifier. Alphanumeric code that identifies the specific transaction",required=true) 
     		@PathVariable("transferid") String transferId);
+	
+	@Operation(summary = "Return the list of the organizations.", security = {@SecurityRequirement(name = "ApiKey"), @SecurityRequirement(name = "Authorization")}, operationId = "getOrganizations", tags={"Get Organization List"})
+	@ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Obtained organizations to add and delete.", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = OrganizationListModelResponse.class))),
+            @ApiResponse(responseCode = "401", description = "Wrong or missing function key.", content = @Content(schema = @Schema())),
+            @ApiResponse(responseCode = "500", description = "Service unavailable.", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ProblemJson.class)))})
+	@GetMapping(value = "/organizations",
+	produces = { "application/json" })
+	public ResponseEntity<OrganizationListModelResponse> getOrganizations(
+            @Valid @Parameter(description = "Filter from date (use the format yyyy-MM-dd)") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @RequestParam(value = "since", required = true)   
+            LocalDate since);   
 
 }
---
+++ gpd/src/main/java/it/gov/pagopa/debtposition/model/payments/response/OrganizationListModelResponse.java
@@ -0,0 +1,26 @@
+package it.gov.pagopa.debtposition.model.payments.response;
+
+import java.io.Serializable;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder (toBuilder = true)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrganizationListModelResponse implements Serializable {
+
+	/**
+     * generated serialVersionUID
+     */
+	private static final long serialVersionUID = -5630972730850116336L;
+	
+    
+	private List<OrganizationModelResponse> add;
+	private List<OrganizationModelResponse> delete;
+	
+}
