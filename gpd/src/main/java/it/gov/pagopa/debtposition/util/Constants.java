53366fd5c23bad0075b372950fe64618536a7b27
aacitelli <aacitelli@PDD-NB-0136.dgsgroup.it>
2022-03-01 10:20:10 +0100

    [PPD-85] swagger: aggiornato file openapi

--- gpd/src/main/java/it/gov/pagopa/debtposition/util/Constants.java
+++ gpd/src/main/java/it/gov/pagopa/debtposition/util/Constants.java
@@ -1,11 +1,13 @@
 package it.gov.pagopa.debtposition.util;
 
-public class HttpStatusExplainMessage {
-    private HttpStatusExplainMessage() {
-        super();
-    }
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class Constants {
 
     public static final String DEBT_POSITION_DELETED      = "The debt position was successfully deleted";
     public static final String DEBT_POSITION_PUBLISHED    = "The debt position was successfully published";
     public static final String DEBT_POSITION_INVALIDATED  = "The debt position was successfully invalidated";
+    
+    public static final String HEADER_REQUEST_ID = "X-Request-Id";
 }
