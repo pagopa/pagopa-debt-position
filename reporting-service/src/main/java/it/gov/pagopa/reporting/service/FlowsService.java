c258721856e8b322ffe751b3d476f665330ee645
Francesco Cesareo <cesareo.francesco@gmail.com>
2022-03-09 22:50:46 +0100

    unit test improved

--- reporting-service/src/main/java/it/gov/pagopa/reporting/service/FlowsService.java
+++ reporting-service/src/main/java/it/gov/pagopa/reporting/service/FlowsService.java
@@ -2,7 +2,6 @@ package it.gov.pagopa.reporting.service;
 
 import java.net.URISyntaxException;
 import java.security.InvalidKeyException;
-import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -67,79 +66,80 @@ public class FlowsService {
         AzuriteStorageUtil azuriteStorageUtil = new AzuriteStorageUtil(storageConnectionString, null, null, containerBlob);
         azuriteStorageUtil.createBlob();
 
-        try {
-            NodeService nodeService = this.getNodeServiceInstance();
+        NodeService nodeService = this.getNodeServiceInstance();
 
-            IntStream.range(0, flows.size()).forEach(index -> {
-                TipoIdRendicontazione flow = flows.get(index);
-                try {
-                    // nodoChiediFlussoRendicontazione(idPA, idFlow)
-                    // https://github.com/pagopa/pagopa-api/blob/master/nodo/NodoPerPa.wsdl#L523
-                    nodeService.callNodoChiediFlussoRendicontazione(idPA, flow.getIdentificativoFlusso());
+        IntStream.range(0, flows.size()).forEach(index -> {
+            TipoIdRendicontazione flow = flows.get(index);
+            try {
+                // nodoChiediFlussoRendicontazione(idPA, idFlow)
+                // https://github.com/pagopa/pagopa-api/blob/master/nodo/NodoPerPa.wsdl#L523
+                nodeService.callNodoChiediFlussoRendicontazione(idPA, flow.getIdentificativoFlusso());
 
-                    FaultBean faultBean = nodeService.getNodoChiediFlussoRendicontazioneFault();
-                    DataHandler xmlReporting = nodeService.getNodoChiediElencoFlussiRendicontazioneXmlReporting();
+                FaultBean faultBean = nodeService.getNodoChiediFlussoRendicontazioneFault();
+                DataHandler xmlReporting = nodeService.getNodoChiediElencoFlussiRendicontazioneXmlReporting();
 
-                    if (faultBean != null) {
-                        logger.log(Level.INFO,
-                                () -> "[RetrieveDetails/FlowsService] faultBean DESC " + faultBean.getDescription());
-                        // TODO to analyze what should be done
-                    } else if (xmlReporting != null) {
+                if (faultBean != null) {
+                    logger.log(Level.SEVERE,
+                            () -> "[RetrieveDetails/FlowsService] faultBean DESC " + faultBean.getDescription());
+                    // TODO to analyze what should be done
+                } else if (xmlReporting != null) {
+                    saveBlob(flow, idPA, xmlReporting);
 
-                        BlobServiceClient blobServiceClient = new BlobServiceClientBuilder()
-                                .connectionString(this.storageConnectionString).buildClient();
-
-                        BlobContainerClient flowsContainerClient = blobServiceClient.getBlobContainerClient(this.containerBlob);
-
-                        // dataOra##idPa##idflow.xml
-                        // added split to remove millis from name
-                        BlobClient blobClient = flowsContainerClient.getBlobClient(
-                                flow.getDataOraFlusso().toString().split("\\.")[0] + "##" + idPA + "##" + flow.getIdentificativoFlusso() + ".xml"
-                        );
-
-                        logger.log(Level.INFO, () ->
-                                "[RetrieveDetails/FlowsService] Uploading " + blobClient.getBlobUrl() + " in " + this.containerBlob);
-
-                        try {
-                            // save base64 as xml string
-                            blobClient.upload(BinaryData.fromStream(xmlReporting.getInputStream()));
-                        } catch (IOException e) {
-                            logger.log(Level.SEVERE, () -> "[RetrieveDetails/FlowsService] Upload failed "
-                                    + blobClient.getBlobUrl() + " in " + this.containerBlob);
-                        }
-
-                        logger.log(Level.INFO, () ->
-                                "[RetrieveDetails/FlowsService] Uploaded " + blobClient.getBlobUrl() + " in " + this.containerBlob);
-                    }
-                } catch (ClientTransportException e) {
-                    logger.log(Level.SEVERE, () -> "[NODO Connection down] " + idPA + " - " + flow.getIdentificativoFlusso());
-
-                    if (retry < maxRetryQueuing) {
-                        try {
-                            CloudQueue queue = CloudStorageAccount.parse(storageConnectionString).createCloudQueueClient()
-                                    .getQueueReference(this.flowsQueue);
-
-                            FlowsMessage fm = new FlowsMessage();
-                            fm.setFlows(new TipoIdRendicontazione[]{flow});
-                            fm.setIdPA(idPA);
-                            fm.setRetry(retry);
-                            String message = new ObjectMapper().writeValueAsString(fm);
-                            queue.addMessage(new CloudQueueMessage(message), timeToLiveInSeconds, initialVisibilityDelayInSeconds, null, null);
-                        } catch (URISyntaxException | StorageException | InvalidKeyException | JsonProcessingException ex) {
-                            ex.printStackTrace();
-                            logger.log(Level.SEVERE, () -> "[RetrieveDetails/FlowsService] Problem to re-queuing: " + idPA + " - " + flow.getIdentificativoFlusso());
-                        }
-                    } else {
-                        logger.log(Level.SEVERE, () -> "[NODO Connection down]  Max retry exceeded.");
-                    }
+                    logger.log(Level.INFO, () ->
+                            "[RetrieveDetails/FlowsService] Uploaded in " + this.containerBlob);
                 }
-            });
+            }
+            catch (IOException e) {
+                logger.log(Level.SEVERE, () -> "[RetrieveDetails/FlowsService] Upload failed in " + this.containerBlob);
+            }
+            catch (ClientTransportException e) {
+                logger.log(Level.SEVERE, () -> "[NODO Connection down] " + idPA + " - " + flow.getIdentificativoFlusso());
 
-        } catch (Exception e) {
-            logger.log(Level.SEVERE, () -> "[RetrieveDetails/FlowsService] Generic Error " + e.getMessage());
-        }
+                if (retry < maxRetryQueuing) {
+                    try {
+                        reQueuingMessage(idPA, flow, retry);
+                    } catch (URISyntaxException | StorageException | InvalidKeyException | JsonProcessingException ex) {
+                        logger.log(Level.SEVERE, () -> "[RetrieveDetails/FlowsService] Problem to re-queuing: " + idPA + " - " + flow.getIdentificativoFlusso());
+                    }
+                } else {
+                    logger.log(Level.SEVERE, () -> "[NODO Connection down]  Max retry exceeded.");
+                }
+            }
+        });
 
         this.logger.log(Level.INFO, "[RetrieveDetails/FlowsService] END flows storing ");
+    }
+
+    protected void saveBlob(TipoIdRendicontazione flow, String idPA, DataHandler xmlReporting) throws IOException {
+        BlobServiceClient blobServiceClient = new BlobServiceClientBuilder()
+                .connectionString(this.storageConnectionString).buildClient();
+
+        BlobContainerClient flowsContainerClient = blobServiceClient.getBlobContainerClient(this.containerBlob);
+
+        // dataOra##idPa##idflow.xml
+        // added split to remove millis from name
+        BlobClient blobClient = flowsContainerClient.getBlobClient(
+                flow.getDataOraFlusso().toString().split("\\.")[0] + "##" + idPA + "##" + flow.getIdentificativoFlusso() + ".xml"
+        );
+
+        logger.log(Level.INFO, () ->
+                "[RetrieveDetails/FlowsService] Uploading " + blobClient.getBlobUrl() + " in " + this.containerBlob);
+
+
+        // save base64 as xml string
+        blobClient.upload(BinaryData.fromStream(xmlReporting.getInputStream()));
+    }
+
+    protected void reQueuingMessage(String idPA, TipoIdRendicontazione flow, int retry) throws URISyntaxException, InvalidKeyException, StorageException, JsonProcessingException {
+        CloudQueue queue = CloudStorageAccount.parse(storageConnectionString).createCloudQueueClient()
+                .getQueueReference(this.flowsQueue);
+
+        FlowsMessage fm = new FlowsMessage();
+        fm.setFlows(new TipoIdRendicontazione[]{flow});
+        fm.setIdPA(idPA);
+        fm.setRetry(retry);
+        String message = new ObjectMapper().writeValueAsString(fm);
+        queue.addMessage(new CloudQueueMessage(message), timeToLiveInSeconds, initialVisibilityDelayInSeconds, null, null);
     }
 
     public NodeService getNodeServiceInstance() {
--- reporting-service/src/test/java/it/gov/pagopa/reporting/service/FlowServiceIntegrationTest.java
+++ reporting-service/src/test/java/it/gov/pagopa/reporting/service/FlowServiceIntegrationTest.java
@@ -1,20 +1,15 @@
 package it.gov.pagopa.reporting.service;
 
-import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.spy;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
+import java.io.IOException;
 import java.net.URISyntaxException;
 import java.security.InvalidKeyException;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
@@ -27,8 +22,13 @@ import com.azure.storage.blob.BlobServiceClient;
 import com.azure.storage.blob.BlobServiceClientBuilder;
 import com.azure.storage.blob.models.BlobItem;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.microsoft.azure.storage.StorageException;
+import com.sun.xml.ws.client.ClientTransportException;
 import org.junit.ClassRule;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -40,105 +40,181 @@ import it.gov.pagopa.reporting.servicewsdl.TipoIdRendicontazione;
 @Testcontainers
 class FlowServiceIntegrationTest {
 
-        @ClassRule
-        @Container
-        public static GenericContainer<?> azurite = new GenericContainer<>(
-                        DockerImageName.parse("mcr.microsoft.com/azure-storage/azurite:latest")).withExposedPorts(10001,
-                                        10002, 10000);
+    @ClassRule
+    @Container
+    public static GenericContainer<?> azurite = new GenericContainer<>(
+            DockerImageName.parse("mcr.microsoft.com/azure-storage/azurite:latest")).withExposedPorts(10001,
+            10002, 10000);
 
-        Logger logger = Logger.getLogger("testlogging");
+    Logger logger = Logger.getLogger("testlogging");
 
-        String storageConnectionString = String.format(
-                        "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://%s:%s/devstoreaccount1;QueueEndpoint=http://%s:%s/devstoreaccount1",
-                        azurite.getContainerIpAddress(), azurite.getMappedPort(10000), azurite.getContainerIpAddress(),
-                        azurite.getMappedPort(10001));
-        String flowsTable = "testtable";
-        String flowsQueue = "testqueue";
+    String storageConnectionString = String.format(
+            "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://%s:%s/devstoreaccount1;QueueEndpoint=http://%s:%s/devstoreaccount1",
+            azurite.getContainerIpAddress(), azurite.getMappedPort(10000), azurite.getContainerIpAddress(),
+            azurite.getMappedPort(10001));
+    String flowsTable = "testtable";
+    String flowsQueue = "testqueue";
 
-        FlowsService flowsService;
+    FlowsService flowsService;
 
-        @Test
-        void flowsBatchProcessingTest()
-                        throws ParseException, DatatypeConfigurationException, InvalidKeyException, URISyntaxException {
+    @Test
+    void flowsBatchProcessingTest()
+            throws ParseException, DatatypeConfigurationException, InvalidKeyException, URISyntaxException {
 
-                flowsService = spy(new FlowsService(storageConnectionString, "identificativoIntemediarioPA",
-                                "identificativoStazioneIntermediarioPA", "nodePassword",
-                                "container", "queue", 1, 60, 0, logger));
+        flowsService = spy(new FlowsService(storageConnectionString, "identificativoIntemediarioPA",
+                "identificativoStazioneIntermediarioPA", "nodePassword",
+                "container", "queue", 1, 60, 0, logger));
 
-                NodeService nodeService = mock(NodeService.class);
-                doReturn(mock(DataHandler.class)).when(nodeService)
-                                .getNodoChiediElencoFlussiRendicontazioneXmlReporting();
+        NodeService nodeService = mock(NodeService.class);
+        doReturn(mock(DataHandler.class)).when(nodeService)
+                .getNodoChiediElencoFlussiRendicontazioneXmlReporting();
 
-                doReturn(nodeService).when(flowsService).getNodeServiceInstance();
+        doReturn(nodeService).when(flowsService).getNodeServiceInstance();
 
-                BlobServiceClient blobServiceClient = new BlobServiceClientBuilder()
-                                .connectionString(this.storageConnectionString).buildClient();
+        BlobServiceClient blobServiceClient = new BlobServiceClientBuilder()
+                .connectionString(this.storageConnectionString).buildClient();
 
-                BlobContainerClient containerClient = blobServiceClient.createBlobContainer("container");
+        BlobContainerClient containerClient = blobServiceClient.createBlobContainer("container");
 
-                DateFormat format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-                Date date1 = format.parse("2014-04-24 11:15:00");
-                GregorianCalendar cal1 = new GregorianCalendar();
-                cal1.setTime(date1);
-                TipoIdRendicontazione e1 = new TipoIdRendicontazione();
-                String id1 = UUID.randomUUID().toString();
-                e1.setIdentificativoFlusso(id1);
-                e1.setDataOraFlusso(DatatypeFactory.newInstance().newXMLGregorianCalendar(
-                                DatatypeFactory.newInstance().newXMLGregorianCalendar(cal1).toGregorianCalendar()));
+        DateFormat format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        Date date1 = format.parse("2014-04-24 11:15:00");
+        GregorianCalendar cal1 = new GregorianCalendar();
+        cal1.setTime(date1);
+        TipoIdRendicontazione e1 = new TipoIdRendicontazione();
+        String id1 = UUID.randomUUID().toString();
+        e1.setIdentificativoFlusso(id1);
+        e1.setDataOraFlusso(DatatypeFactory.newInstance().newXMLGregorianCalendar(
+                DatatypeFactory.newInstance().newXMLGregorianCalendar(cal1).toGregorianCalendar()));
 
-                TipoIdRendicontazione e2 = new TipoIdRendicontazione();
-                String id2 = UUID.randomUUID().toString();
-                e2.setIdentificativoFlusso(id2);
-                e2.setDataOraFlusso(DatatypeFactory.newInstance().newXMLGregorianCalendar(
-                                DatatypeFactory.newInstance().newXMLGregorianCalendar(cal1).toGregorianCalendar()));
+        TipoIdRendicontazione e2 = new TipoIdRendicontazione();
+        String id2 = UUID.randomUUID().toString();
+        e2.setIdentificativoFlusso(id2);
+        e2.setDataOraFlusso(DatatypeFactory.newInstance().newXMLGregorianCalendar(
+                DatatypeFactory.newInstance().newXMLGregorianCalendar(cal1).toGregorianCalendar()));
 
-                List<TipoIdRendicontazione> flows = Arrays.asList(e1, e2);
+        List<TipoIdRendicontazione> flows = Arrays.asList(e1, e2);
 
-                List<String> flowsFileName = Arrays.asList(
-                        e1.getDataOraFlusso().toString().split("\\.")[0] + "##" + "idPA" + "##"+ e1.getIdentificativoFlusso() + ".xml",
-                        e2.getDataOraFlusso().toString().split("\\.")[0] + "##" + "idPA" + "##"+ e2.getIdentificativoFlusso() + ".xml");
+        List<String> flowsFileName = Arrays.asList(
+                e1.getDataOraFlusso().toString().split("\\.")[0] + "##" + "idPA" + "##" + e1.getIdentificativoFlusso() + ".xml",
+                e2.getDataOraFlusso().toString().split("\\.")[0] + "##" + "idPA" + "##" + e2.getIdentificativoFlusso() + ".xml");
 
-                flowsService.flowsXmlDownloading(flows, "idPA", 0);
+        flowsService.flowsXmlDownloading(flows, "idPA", 0);
 
-                List<String> fileNames = containerClient.listBlobs().stream().map(BlobItem::getName)
-                                .collect(Collectors.toList());
+        List<String> fileNames = containerClient.listBlobs().stream().map(BlobItem::getName)
+                .collect(Collectors.toList());
 
-                assertTrue(fileNames.containsAll(flowsFileName));
-        }
+        assertTrue(fileNames.containsAll(flowsFileName));
+    }
 
-        @Test
-        void flowsProcessingErrorNodeTest()
-                        throws ParseException, DatatypeConfigurationException, InvalidKeyException, URISyntaxException {
+    @Test
+    void flowsProcessingErrorNodeTest()
+            throws ParseException, DatatypeConfigurationException, InvalidKeyException, URISyntaxException {
 
-                flowsService = spy(new FlowsService(storageConnectionString, "identificativoIntemediarioPA",
-                                "identificativoStazioneIntermediarioPA", "nodePassword",
-                                "container", "queue", 1, 60, 0, logger));
+        flowsService = spy(new FlowsService(storageConnectionString, "identificativoIntemediarioPA",
+                "identificativoStazioneIntermediarioPA", "nodePassword",
+                "container", "queue", 1, 60, 0, logger));
 
-                NodeService nodeService = mock(NodeService.class);
-                doReturn(mock(FaultBean.class)).when(nodeService).getNodoChiediFlussoRendicontazioneFault();
+        NodeService nodeService = mock(NodeService.class);
+        doReturn(mock(FaultBean.class)).when(nodeService).getNodoChiediFlussoRendicontazioneFault();
 
-                doReturn(nodeService).when(flowsService).getNodeServiceInstance();
+        doReturn(nodeService).when(flowsService).getNodeServiceInstance();
 
-                DateFormat format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-                Date date1 = format.parse("2014-04-24 11:15:00");
-                GregorianCalendar cal1 = new GregorianCalendar();
-                cal1.setTime(date1);
-                TipoIdRendicontazione e1 = new TipoIdRendicontazione();
-                String id1 = UUID.randomUUID().toString();
-                e1.setIdentificativoFlusso(id1);
-                e1.setDataOraFlusso(DatatypeFactory.newInstance().newXMLGregorianCalendar(
-                                DatatypeFactory.newInstance().newXMLGregorianCalendar(cal1).toGregorianCalendar()));
+        DateFormat format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        Date date1 = format.parse("2014-04-24 11:15:00");
+        GregorianCalendar cal1 = new GregorianCalendar();
+        cal1.setTime(date1);
+        TipoIdRendicontazione e1 = new TipoIdRendicontazione();
+        String id1 = UUID.randomUUID().toString();
+        e1.setIdentificativoFlusso(id1);
+        e1.setDataOraFlusso(DatatypeFactory.newInstance().newXMLGregorianCalendar(
+                DatatypeFactory.newInstance().newXMLGregorianCalendar(cal1).toGregorianCalendar()));
 
-                TipoIdRendicontazione e2 = new TipoIdRendicontazione();
-                String id2 = UUID.randomUUID().toString();
-                e2.setIdentificativoFlusso(id2);
-                e2.setDataOraFlusso(DatatypeFactory.newInstance().newXMLGregorianCalendar(
-                                DatatypeFactory.newInstance().newXMLGregorianCalendar(cal1).toGregorianCalendar()));
+        TipoIdRendicontazione e2 = new TipoIdRendicontazione();
+        String id2 = UUID.randomUUID().toString();
+        e2.setIdentificativoFlusso(id2);
+        e2.setDataOraFlusso(DatatypeFactory.newInstance().newXMLGregorianCalendar(
+                DatatypeFactory.newInstance().newXMLGregorianCalendar(cal1).toGregorianCalendar()));
 
-                List<TipoIdRendicontazione> flows = Arrays.asList(e1, e2);
+        List<TipoIdRendicontazione> flows = Arrays.asList(e1, e2);
 
-                flowsService.flowsXmlDownloading(flows, "idPA", 0);
+        flowsService.flowsXmlDownloading(flows, "idPA", 0);
 
-                assertTrue(Boolean.TRUE);
-        }
+        assertTrue(Boolean.TRUE);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1})
+    void flowsProcessingNodeKOTest(int maxRetry)
+            throws ParseException, DatatypeConfigurationException, InvalidKeyException, URISyntaxException, StorageException, JsonProcessingException {
+
+        flowsService = spy(new FlowsService(storageConnectionString, "identificativoIntemediarioPA",
+                "identificativoStazioneIntermediarioPA", "nodePassword",
+                "container", "queue", maxRetry, 60, 0, logger));
+
+        NodeService nodeService = mock(NodeService.class);
+
+        doThrow(ClientTransportException.class).when(nodeService).callNodoChiediFlussoRendicontazione(anyString(), anyString());
+
+        doReturn(nodeService).when(flowsService).getNodeServiceInstance();
+
+        DateFormat format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        Date date1 = format.parse("2014-04-24 11:15:00");
+        GregorianCalendar cal1 = new GregorianCalendar();
+        cal1.setTime(date1);
+        TipoIdRendicontazione e1 = new TipoIdRendicontazione();
+        String id1 = UUID.randomUUID().toString();
+        e1.setIdentificativoFlusso(id1);
+        e1.setDataOraFlusso(DatatypeFactory.newInstance().newXMLGregorianCalendar(
+                DatatypeFactory.newInstance().newXMLGregorianCalendar(cal1).toGregorianCalendar()));
+
+        TipoIdRendicontazione e2 = new TipoIdRendicontazione();
+        String id2 = UUID.randomUUID().toString();
+        e2.setIdentificativoFlusso(id2);
+        e2.setDataOraFlusso(DatatypeFactory.newInstance().newXMLGregorianCalendar(
+                DatatypeFactory.newInstance().newXMLGregorianCalendar(cal1).toGregorianCalendar()));
+
+        List<TipoIdRendicontazione> flows = Arrays.asList(e1, e2);
+
+        flowsService.flowsXmlDownloading(flows, "idPA", 0);
+
+        int times = maxRetry == 1 ? 2 : 0;
+        verify(flowsService, times(times)).reQueuingMessage(anyString(), any(), anyInt());
+    }
+
+    @Test
+    void flowsProcessingNodeKOTest_2()
+            throws ParseException, DatatypeConfigurationException, InvalidKeyException, URISyntaxException, StorageException, IOException {
+
+        flowsService = spy(new FlowsService(storageConnectionString, "identificativoIntemediarioPA",
+                "identificativoStazioneIntermediarioPA", "nodePassword",
+                "container", "queue", 1, 60, 0, logger));
+
+        NodeService nodeService = mock(NodeService.class);
+        doReturn(mock(DataHandler.class)).when(nodeService).getNodoChiediElencoFlussiRendicontazioneXmlReporting();
+        doThrow(IOException.class).when(flowsService).saveBlob(any(), anyString(), any());
+
+        doReturn(nodeService).when(flowsService).getNodeServiceInstance();
+
+        DateFormat format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        Date date1 = format.parse("2014-04-24 11:15:00");
+        GregorianCalendar cal1 = new GregorianCalendar();
+        cal1.setTime(date1);
+        TipoIdRendicontazione e1 = new TipoIdRendicontazione();
+        String id1 = UUID.randomUUID().toString();
+        e1.setIdentificativoFlusso(id1);
+        e1.setDataOraFlusso(DatatypeFactory.newInstance().newXMLGregorianCalendar(
+                DatatypeFactory.newInstance().newXMLGregorianCalendar(cal1).toGregorianCalendar()));
+
+        TipoIdRendicontazione e2 = new TipoIdRendicontazione();
+        String id2 = UUID.randomUUID().toString();
+        e2.setIdentificativoFlusso(id2);
+        e2.setDataOraFlusso(DatatypeFactory.newInstance().newXMLGregorianCalendar(
+                DatatypeFactory.newInstance().newXMLGregorianCalendar(cal1).toGregorianCalendar()));
+
+        List<TipoIdRendicontazione> flows = Arrays.asList(e1, e2);
+
+        flowsService.flowsXmlDownloading(flows, "idPA", 0);
+
+        verify(flowsService, times(0)).reQueuingMessage(anyString(), any(), anyInt());
+    }
 }
