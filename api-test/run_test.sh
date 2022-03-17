0d9f0a5646be95bb5c265073b3fa57aa944bee6c
Jacopo <jacopo.carlini@gmail.com>
2022-03-17 11:07:28 +0100

    [PPD-115] postman collection

--- api-test/run_test.sh
+++ api-test/run_test.sh
@@ -33,10 +33,10 @@ else
   if [[ "$EXEC" =~ ^(int|load)$ ]]; then
       if [ "$EXEC" = "load" ]
       then
-          postman-to-k6 api-test/DEBT_POSITION.postman_collection.json --environment api-test/GPD_ENV.postman_environment.json -o ./k6-script.js
+          postman-to-k6 "api-test/Debt Position.postman_collection.json" --environment "api-test/Debt Position - Local.postman_environment.json" -o ./k6-script.js
           k6 run --http-debug="full" --out csv=test.csv --vus 1 --duration 1s ./k6-script.js
       else
-          newman run api-test/DEBT_POSITION.postman_collection.json --environment=api-test/GPD_ENV.postman_environment.json --reporters cli,junit --reporter-junit-export Results/GPD-TEST.xml
+          newman run "api-test/Debt Position.postman_collection.json" --environment="api-test/Debt Position - Local.postman_environment.json" --reporters cli,junit --reporter-junit-export Results/DebtPosition-TEST.xml
       fi
   else
       echo "Warning: No test run : select STAGE (d=DEV,u=UAT,p=PROD,l=local) and ENV (int=integration,load=k6) see example below !"
