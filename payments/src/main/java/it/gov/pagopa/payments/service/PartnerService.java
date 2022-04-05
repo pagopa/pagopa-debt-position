7fba2356e4072b40731d3a88b65f358a2039edcd
pasqualespica <36746022+pasqualespica@users.noreply.github.com>
2022-04-05 17:50:26 +0200

    fix iuv in to activare response

--- payments/src/main/java/it/gov/pagopa/payments/service/PartnerService.java
+++ payments/src/main/java/it/gov/pagopa/payments/service/PartnerService.java
@@ -248,7 +248,7 @@ public class PartnerService {
         response.setOutcome(StOutcome.OK);
 
         // general payment data
-        responseData.setCreditorReferenceId(source.getOrganizationFiscalCode());
+        responseData.setCreditorReferenceId(request.getQrCode().getNoticeNumber().substring(1)); // set IUV from notice number request
         responseData.setPaymentAmount(BigDecimal.valueOf(source.getAmount()));
         responseData.setDueDate(DatatypeFactory.newInstance().newXMLGregorianCalendar(source.getDueDate().toString()));
         responseData.setRetentionDate(source.getRetentionDate() != null
