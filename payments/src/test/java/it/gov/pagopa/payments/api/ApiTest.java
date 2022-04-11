9fc58ddaae32d763faed850dca4498e026383cfc
aacitelli <aacitelli@host.docker.internal>
2022-04-11 09:52:03 +0200

    [PDD 136] receipt api: corrected error in junit ApiTest class

--- payments/src/test/java/it/gov/pagopa/payments/api/ApiTest.java
+++ payments/src/test/java/it/gov/pagopa/payments/api/ApiTest.java
@@ -1,19 +1,22 @@
 package it.gov.pagopa.payments.api;
 
-import it.gov.pagopa.payments.PaymentsApplication;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
-
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import it.gov.pagopa.payments.PaymentsApplication;
+
+@SpringBootTest(classes = PaymentsApplication.class)
 @ExtendWith(SpringExtension.class)
-@WebMvcTest
+@AutoConfigureMockMvc
 class ApiTest {
 
     @Autowired
@@ -27,12 +30,6 @@ class ApiTest {
 
     @Test
     void applicationContextLoaded() {
-        assertTrue(true); // it just tests that an error has not occurred
-    }
-
-    @Test
-    void applicationContextTest() {
-        PaymentsApplication.main(new String[]{});
         assertTrue(true); // it just tests that an error has not occurred
     }
 }
