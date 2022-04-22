9fad9dcbc504a3b8829a78c0e9de501b4ac3f25f
Jacopo <jacopo.carlini@gmail.com>
2022-04-22 15:30:41 +0200

    [PPD-152] added cache to payments

--- payments/src/main/java/it/gov/pagopa/payments/PaymentsApplication.java
+++ payments/src/main/java/it/gov/pagopa/payments/PaymentsApplication.java
@@ -2,12 +2,14 @@ package it.gov.pagopa.payments;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableRetry
+@EnableCaching
 public class PaymentsApplication {
 
     public static void main(final String[] args) {
--- payments/src/main/java/it/gov/pagopa/payments/service/GpdClient.java
+++ payments/src/main/java/it/gov/pagopa/payments/service/GpdClient.java
@@ -1,5 +1,10 @@
 package it.gov.pagopa.payments.service;
 
+import feign.FeignException;
+import feign.RequestInterceptor;
+import it.gov.pagopa.payments.model.PaymentOptionModel;
+import it.gov.pagopa.payments.model.PaymentOptionModelResponse;
+import it.gov.pagopa.payments.model.PaymentsModelResponse;
 import org.slf4j.MDC;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -13,17 +18,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-import feign.FeignException;
-import feign.RequestInterceptor;
-import it.gov.pagopa.payments.model.PaymentOptionModel;
-import it.gov.pagopa.payments.model.PaymentOptionModelResponse;
-import it.gov.pagopa.payments.model.PaymentsModelResponse;
-
 
 @FeignClient(value = "gpd", url = "${service.gpd.host}", configuration = GpdClient.FeignConfiguration.class)
 public interface GpdClient {
 
-    @Cacheable("getOrganization")
+    @Cacheable(value = "payment-options")
     @Retryable(exclude = FeignException.FeignClientException.class, maxAttemptsExpression = "${retry.maxAttempts}",
             backoff = @Backoff(delayExpression = "${retry.maxDelay}"))
     @GetMapping(value = "/organizations/{organizationfiscalcode}")
