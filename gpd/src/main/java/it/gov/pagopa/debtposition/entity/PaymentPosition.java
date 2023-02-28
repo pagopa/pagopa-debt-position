64fd7e02ff96400ae6abf3a4270b7006f46bc942
FedericoRuzzier <49512050+FedericoRuzzier@users.noreply.github.com>
2023-02-28 17:22:02 +0100

    PAGOPA-705 update query

--- gpd/src/main/java/it/gov/pagopa/debtposition/entity/PaymentPosition.java
+++ gpd/src/main/java/it/gov/pagopa/debtposition/entity/PaymentPosition.java
@@ -8,20 +8,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.SequenceGenerator;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
-import javax.persistence.Version;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.time.LocalDateTime;
@@ -42,6 +29,8 @@ import java.util.List;
 @Entity
 @Table(name = "payment_position", uniqueConstraints = {
         @UniqueConstraint(name = "UniquePaymentPos", columnNames = {"iupd", "organization_fiscal_code"})
+}, indexes = {
+        @Index(name = "multiIndex", columnList = "organization_fiscal_code, iupd")
 })
 public class PaymentPosition implements Serializable {
 
