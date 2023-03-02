7228ab9ac301ad7632456b9d50a09d629c4a2631
FedericoRuzzier <49512050+FedericoRuzzier@users.noreply.github.com>
2023-03-02 12:28:15 +0100

    PAGOPA-639 adding test for coverage

--- gpd/src/test/java/it/gov/pagopa/debtposition/controller/DebtPositionActionsControllerTest.java
+++ gpd/src/test/java/it/gov/pagopa/debtposition/controller/DebtPositionActionsControllerTest.java
@@ -167,6 +167,32 @@ class DebtPositionActionsControllerTest {
 
 	}
 
+	@Test
+	void publishDebtPosition_min_valid_date_409() throws Exception {
+
+		// creo una posizione debitoria (con 'validity date' impostata) e con due_date di pochissimo più grande della current_date
+		mvc.perform(post("/organizations/PBH409DUEDATE_12345678901/debtpositions")
+						.content(TestUtil.toJson(DebtPositionMock.get409_Valid_Date_Mock1())).contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isCreated());
+
+		// recupero la posizione debitoria e verifico lo stato
+		mvc.perform(get("/organizations/PBH409DUEDATE_12345678901/debtpositions/12345678901IUPDMOCK3")
+						.contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.status")
+						.value(DebtPositionStatus.DRAFT.toString()))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.publishDate").isEmpty());
+
+		// introduco un ritardo in modo da far scadere la validity_date
+		LocalDateTime currentDatePlusSeconds = LocalDateTime.now(ZoneOffset.UTC).plus(5, ChronoUnit.SECONDS);
+		Awaitility.await().until(() -> LocalDateTime.now(ZoneOffset.UTC).isAfter(currentDatePlusSeconds));
+
+		// porto in pubblicata lo stato della posizione debitoria => devo ottenere 409 per il fatto che currentDate > due_date
+		mvc.perform(post("/organizations/PBH409DUEDATE_12345678901/debtpositions/12345678901IUPDMOCK3/publish")
+				.contentType(MediaType.APPLICATION_JSON)).andExpect(status().isConflict());
+
+	}
+
 
 	/**
 	 * INVALIDATE DEBT POSITION
