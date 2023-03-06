6501b5af5027e2a62db07859baa428b9310cadd1
FedericoRuzzier <49512050+FedericoRuzzier@users.noreply.github.com>
2023-03-06 15:11:43 +0100

    PAGOPA-737 readding gpd client

---
+++ gpd/integration-test/src/step_definitions/support/clients/gpd_client.js
@@ -0,0 +1,38 @@
+const {get, post} = require("../utility/axios_common");
+const fs = require("fs");
+
+const gpd_host = process.env.gpd_host;
+
+function gpdHealthCheck() {
+    return get(gpd_host + `/info`, {
+        headers: {
+            "Ocp-Apim-Subscription-Key": process.env.GPD_SUBSCRIPTION_KEY
+        }
+    })
+}
+
+function createDebtPosition(orgId, body) {
+    return post(gpd_host + `/organizations/${orgId}/debtpositions`, body, {
+        timeout: 10000,
+        headers: {
+            "Ocp-Apim-Subscription-Key": process.env.GPD_SUBSCRIPTION_KEY,
+            "Content-Type": "application/json"
+        }
+    })
+}
+
+function publishDebtPosition(orgId, iupd) {
+    return post(gpd_host + `/organizations/${orgId}/debtpositions/${iupd}/publish`, "", {
+        timeout: 10000,
+        headers: {
+            "Ocp-Apim-Subscription-Key": process.env.GPD_SUBSCRIPTION_KEY,
+            "Content-Type": "application/json"
+        }
+    })
+}
+
+module.exports = {
+    createDebtPosition,
+    gpdHealthCheck,
+    publishDebtPosition
+}
