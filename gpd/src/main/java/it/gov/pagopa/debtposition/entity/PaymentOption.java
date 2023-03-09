9e60565719fc015c505aebb9a0999d92ac7ac9b8
FedericoRuzzier <49512050+FedericoRuzzier@users.noreply.github.com>
2023-03-09 15:19:55 +0100

    adding indexes for transfer and payment position

--- gpd/src/main/java/it/gov/pagopa/debtposition/entity/PaymentOption.java
+++ gpd/src/main/java/it/gov/pagopa/debtposition/entity/PaymentOption.java
@@ -7,21 +7,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.SequenceGenerator;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.time.LocalDateTime;
@@ -44,7 +30,8 @@ import java.util.List;
                 @UniqueConstraint(
                         name = "UniquePaymentOpt",
                         columnNames = {"iuv", "organization_fiscal_code"})
-        })
+        },
+        indexes = @Index(name = "payment_option_payment_position_id_idx", columnList = "payment_position_id"))
 public class PaymentOption implements Serializable {
 
     /**
