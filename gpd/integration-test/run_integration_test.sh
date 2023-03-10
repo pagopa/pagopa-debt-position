456543d96a79c156772b9207ab1363104f574df8
FedericoRuzzier <49512050+FedericoRuzzier@users.noreply.github.com>
2023-03-10 17:59:19 +0100

    PAGOPA-737 fixing subkey entering in script

--- gpd/integration-test/run_integration_test.sh
+++ gpd/integration-test/run_integration_test.sh
@@ -2,10 +2,10 @@
 set -e
 
 export ENV=$1
-export GPD_SUBSCRIPTION_KEY=$2
+export API_SUBSCRIPTION_KEY=$2
 
 # run integration tests (application must be running)
-export GPD_SUBSCRIPTION_KEY=$2
+
 cd ../integration-test/src || exit
 yarn install
 yarn test:$ENV
