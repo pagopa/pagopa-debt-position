d0bedaf1b5b1568e16d732d6eeafcfc989617014
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2023-03-16 17:55:30 +0100

    [PAGOPA-772] wip

--- reporting-batch/upd_wsdl_location.sh
+++ reporting-batch/upd_wsdl_location.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+echo ${NODO_HOST}
+
 if [ -z ${NODO_HOST} ]; then NODO_HOST="http://localhost:8086/nodo-per-pa/v1"; fi
 
-echo "Defining NODO_HOST as location for SOAP endpoints:" ${NODO_HOST}
+echo "Defining NODO_HOST as location for SOAP endpoints: ${NODO_HOST}"
+
 
 sed "s|NODO_HOST|${NODO_HOST}|g" src/main/resources/wsdl/NodoPerPa.wsdl.TEMPLATE > src/main/resources/wsdl/NodoPerPa.wsdl
