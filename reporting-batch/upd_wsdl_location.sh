37ff0866fee4fa53af237b26f445f6d9b485f418
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2023-03-16 17:39:58 +0100

    [PAGOPA-772] fix: several changes on deploy pipeline

--- reporting-batch/upd_wsdl_location.sh
+++ reporting-batch/upd_wsdl_location.sh
@@ -2,4 +2,6 @@
 
 if [ -z ${NODO_HOST} ]; then NODO_HOST="http://localhost:8086/nodo-per-pa/v1"; fi
 
+echo "Defining NODO_HOST as location for SOAP endpoints:" ${NODO_HOST}
+
 sed "s|NODO_HOST|${NODO_HOST}|g" src/main/resources/wsdl/NodoPerPa.wsdl.TEMPLATE > src/main/resources/wsdl/NodoPerPa.wsdl
