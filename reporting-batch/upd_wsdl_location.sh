2293756cc03d08e743264f0aa1de7b60044803ee
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2023-03-17 17:29:29 +0100

    [PAGOPA-772] fix: Removed Maven exec for autonomous bash execution

--- reporting-batch/upd_wsdl_location.sh
+++ reporting-batch/upd_wsdl_location.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-echo $NODO_HOST
+NODO_HOST=$1
+echo "Passed NODO_HOST parameter value: $NODO_HOST"
 
-if [ -z ${NODO_HOST} ]; then NODO_HOST="http://localhost:8086/nodo-per-pa/v1"; fi
+if [ -z "$NODO_HOST" ]; then
+  echo "No NODO_HOST variable passed. Defining a default value..."
+  NODO_HOST="http://localhost:8086/nodo-per-pa/v1";
+fi
 
-echo "Defining NODO_HOST as location for SOAP endpoints: $NODO_HOST"
+echo "Defined NODO_HOST as location for SOAP endpoints: $NODO_HOST"
 
-
-sed "s|NODO_HOST|${NODO_HOST}|g" src/main/resources/wsdl/NodoPerPa.wsdl.TEMPLATE > src/main/resources/wsdl/NodoPerPa.wsdl
+sed "s|NODO_HOST|$NODO_HOST|g" src/main/resources/wsdl/NodoPerPa.wsdl.TEMPLATE > src/main/resources/wsdl/NodoPerPa.wsdl
