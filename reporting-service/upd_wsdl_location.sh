6d5d32636ae84222b259a6245dde4315a37194fa
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2023-03-20 15:10:40 +0100

    [PAGOPA-772] fix: generalized WSDL location replacing

--- reporting-service/upd_wsdl_location.sh
+++ reporting-service/upd_wsdl_location.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-if [ -z ${NODO_HOST} ]; then NODO_HOST="http://localhost:8086/nodo-per-pa/v1"; fi
+NODO_HOST=$1
+echo "Passed NODO_HOST parameter value: $NODO_HOST"
 
-sed "s|NODO_HOST|${NODO_HOST}|g" src/main/resources/wsdl/NodoPerPa.wsdl.TEMPLATE > src/main/resources/wsdl/NodoPerPa.wsdl
+if [ -z "$NODO_HOST" ]; then
+  echo "No NODO_HOST variable passed. Defining a default value..."
+  NODO_HOST="http://localhost:8086/nodo-per-pa/v1";
+fi
+
+echo "Defined NODO_HOST as location for SOAP endpoints: $NODO_HOST"
+
+sed "s|NODO_HOST|$NODO_HOST|g" src/main/resources/wsdl/NodoPerPa.wsdl.TEMPLATE > src/main/resources/wsdl/NodoPerPa.wsdl
