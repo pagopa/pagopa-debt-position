efe411ebfb833b445024b2af084c27538924cb85
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2023-04-05 16:16:01 +0200

    [PAGOPA-772] fix: reverted refactoring

--- integration-test/run_integration_test.sh
+++ integration-test/run_integration_test.sh
@@ -11,15 +11,14 @@ docker run -dit --name ${containerName} ${CONTAINER_REGISTRY}/yarn-testing-base:
 # run integration tests with yarn
 docker cp -a ./src/. ${containerName}:/test
 docker exec -i ${containerName} /bin/bash -c " \
+cd ./test
 export API_CONFIG_SUBSCRIPTION_KEY=${API_CONFIG_SUBSCRIPTION_KEY} \
 export GPD_SUBSCRIPTION_KEY=${GPD_SUBSCRIPTION_KEY} \
 export PAYMENTS_REST_SUBSCRIPTION_KEY=${PAYMENTS_REST_SUBSCRIPTION_KEY} \
 export PAYMENTS_SOAP_SUBSCRIPTION_KEY=${PAYMENTS_SOAP_SUBSCRIPTION_KEY} \
 export REPORTING_SUBSCRIPTION_KEY=${REPORTING_SUBSCRIPTION_KEY} \
-export REPORTING_BATCH_CONNECTION_STRING=${REPORTING_BATCH_CONNECTION_STRING}"
-
-docker exec -i ${containerName} /bin/bash -c " \
-cd ./test && yarn test:${environment}"
+export REPORTING_BATCH_CONNECTION_STRING=${REPORTING_BATCH_CONNECTION_STRING}
+yarn test:${environment}"
 
 # clean up container
 docker stop ${containerName} && docker rm ${containerName}
