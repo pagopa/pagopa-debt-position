b6faa0737fd88e7a03cab267497c842e7f117b47
AngeloCaporaso <angelo.caporaso@pagopa.it>
2023-07-12 11:27:20 +0200

    [PAGOPA-1039] refactoring: remove unused import

--- src/main/java/it/gov/pagopa/debtposition/model/pd/PaymentOptionModel.java
+++ src/main/java/it/gov/pagopa/debtposition/model/pd/PaymentOptionModel.java
@@ -1,8 +1,6 @@
 package it.gov.pagopa.debtposition.model.pd;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,7 +8,6 @@ import lombok.NoArgsConstructor;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Null;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
