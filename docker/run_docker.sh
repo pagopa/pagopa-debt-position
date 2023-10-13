d5a38035b6adbd459ba7aaf54cbc7760d62816ba
aacitelli <aacitelli@PDD-NB-0205.dgsgroup.it>
2023-10-13 15:23:24 +0200

    [PAGOPA-1292] smoke test: patch to resolve the error

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -47,8 +47,8 @@ docker compose -p "${stack_name}" up -d --remove-orphans --force-recreate --buil
 # waiting the containers
 printf 'Waiting for the service'
 attempt_counter=0
-max_attempts=50
-until $(curl --output /dev/null --silent --head --fail http://localhost:8080/info); do
+max_attempts=75
+until [ $(curl -s -o /dev/null -w "%{http_code}" http://localhost:8080/info) -ne 200 ]; do
     if [ ${attempt_counter} -eq ${max_attempts} ];then
       echo "Max attempts reached"
       exit 1
