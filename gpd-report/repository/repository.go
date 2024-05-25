6ee73f0817547396d1ec1ce34e28603c3d5edadf
AngeloCaporaso <angelo.caporaso@pagopa.it>
2024-05-25 16:19:18 +0200

    fix: Update slack report action

--- gpd-report/repository/repository.go
+++ gpd-report/repository/repository.go
@@ -6,8 +6,9 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"strings"
 
-	_ "github.com/lib/pq"
+	"github.com/lib/pq"
 )
 
 type Repository struct {
@@ -15,9 +16,18 @@ type Repository struct {
 }
 
 func NewRepository() (*Repository, error) {
-	connStr := os.Getenv("PG_CONNECTION_STRING")
-	db, err := sql.Open("postgres", connStr)
+	url := os.Getenv("PG_URL")
+	user := os.Getenv("PG_USER")
+	pass := os.Getenv("PG_PASSWORD")
+	split := strings.Split(url, "/")
+	host := strings.Split(split[2], ":")[0]
+	port := strings.Split(split[2], ":")[1]
+	dbName := strings.Split(split[3], "?")[0]
 
+	connStr := fmt.Sprintf("host=%s port=%s dbname=%s user=%s password=%s sslmode=require", host, port, dbName, user, pass)
+
+	pq.ParseURL(connStr)
+	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		return nil, err
 	}
