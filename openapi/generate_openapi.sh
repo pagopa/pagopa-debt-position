81e2ccfdb054b37f6cf2fd14bc14a749c122b4bc
FedericoRuzzier <49512050+FedericoRuzzier@users.noreply.github.com>
2024-11-13 16:19:18 +0100

    PAGOPA-2197 adapting openapi

--- openapi/generate_openapi.sh
+++ openapi/generate_openapi.sh
@@ -5,3 +5,10 @@ if [[ "$(pwd)" =~ .*"openapi".* ]]; then
 fi
 
 mvn test -Dtest=OpenApiGenerationTest
+
+# deleting bulk update and delete from external json
+jq 'del(.paths["/organizations/{organizationfiscalcode}/debtpositions"] | .put, .delete)' ./openapi/openapi_external.json > ./openapi/openapi_external.json.temp && mv ./openapi/openapi_external.json.temp ./openapi/openapi_external.json
+
+# removing single create and get operations from external massive json
+jq 'del( .paths["/organizations/{organizationfiscalcode}/debtpositions"] | .post, .get ) | .paths["/organizations/{organizationfiscalcode}/debtpositions"].post = .paths["/organizations/{organizationfiscalcode}/debtpositions/bulk"].post | del( .paths["/organizations/{organizationfiscalcode}/debtpositions/bulk"])' ./openapi/openapi_external_massive.json > ./openapi/openapi_external_massive.json.temp && mv ./openapi/openapi_external_massive.json.temp ./openapi/openapi_external_massive.json
+
