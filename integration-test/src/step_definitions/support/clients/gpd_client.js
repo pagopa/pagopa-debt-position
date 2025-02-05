3c3570e96fb7ecead1130e7d0dbefc9271616bed
Jacopo Carlini <jacopo.carlini@pagopa.it>
2025-02-05 15:16:42 +0100

    fix: integration test

--- integration-test/src/step_definitions/support/clients/gpd_client.js
+++ integration-test/src/step_definitions/support/clients/gpd_client.js
@@ -208,7 +208,7 @@ function updateAndPublishDebtPosition(orgId, iupd, body) {
 }
 
 function updateTransferIbanMassive(orgId, oldIban, newIban) {
-    return post(GPD_HOST + `/organizations/${orgId}/transfers/update/iban`, {oldIban, newIban}, {
+    return patch(GPD_HOST + `/organizations/${orgId}/transfers?oldIban=${oldIban}`, {newIban}, {
         timeout: API_TIMEOUT,
         headers: {
             "Ocp-Apim-Subscription-Key": process.env.API_SUBSCRIPTION_KEY,
