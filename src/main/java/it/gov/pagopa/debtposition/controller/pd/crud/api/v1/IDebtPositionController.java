46cda17e4f867b13d8aefa543dd0b591864703d8
svariant <samuele.varianti@nttdata.com>
2025-02-07 11:48:36 +0100

    [PIDM-35] chore: Improve api description

--- src/main/java/it/gov/pagopa/debtposition/controller/pd/crud/api/v1/IDebtPositionController.java
+++ src/main/java/it/gov/pagopa/debtposition/controller/pd/crud/api/v1/IDebtPositionController.java
@@ -664,6 +664,6 @@ public interface IDebtPositionController {
           @PathVariable("organizationfiscalcode")
           String organizationFiscalCode,
       @Parameter(description = "The old iban to replace") @RequestParam @NotBlank String oldIban,
-      @Parameter(description = "Number of Transfer to update, max & default 1000") @Max(1000) @RequestParam(required = false, defaultValue = "1000") int limit,
+      @Parameter(description = "Number of Transfer to update (max = 1000, default = 1000)") @Max(1000) @RequestParam(required = false, defaultValue = "1000") int limit,
       @Valid @RequestBody UpdateTransferIbanMassiveModel updateTransferIbanMassiveModel);
 }
