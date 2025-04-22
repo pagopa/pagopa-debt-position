62808508618e02b0f24e9bc72a070d40dbe232d3
Angelo Caporaso <56113767+cap-ang@users.noreply.github.com>
2025-04-22 10:43:15 +0200

    [PIDM-450] fix(SeND-client): Update Auth Feign Config

--- src/main/java/it/gov/pagopa/debtposition/config/feign/SendFeignConfig.java
+++ src/main/java/it/gov/pagopa/debtposition/config/feign/SendFeignConfig.java
@@ -1,16 +1,29 @@
 package it.gov.pagopa.debtposition.config.feign;
 
+import feign.RequestInterceptor;
+import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
-public class SendFeignConfig extends AuthFeignConfig {
-
+public class SendFeignConfig {
+  static final String HEADER_REQUEST_ID = "X-Request-Id";
+  static final String HEADER_SUBSCRIBTION_KEY = "x-api-key";
   private static final String SEND_SUBKEY_PLACEHOLDER = "${send.subscription-key}";
+  private final String subscriptionKey;
 
   @Autowired
   public SendFeignConfig(@Value(SEND_SUBKEY_PLACEHOLDER) String subscriptionKey) {
     this.subscriptionKey = subscriptionKey;
+  }
+
+  @Bean
+  public RequestInterceptor requestInterceptor() {
+    return requestTemplate ->
+            requestTemplate
+                    .header(HEADER_REQUEST_ID, MDC.get("requestId"))
+                    .header(HEADER_SUBSCRIBTION_KEY, subscriptionKey);
   }
 }
