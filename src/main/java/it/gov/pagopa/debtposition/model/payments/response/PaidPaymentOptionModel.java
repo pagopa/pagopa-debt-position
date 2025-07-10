fde756ebd3b40b2b7560160213896761de665647
Angelo Caporaso <56113767+cap-ang@users.noreply.github.com>
2025-07-10 16:30:58 +0200

    [PAGOPA-3112] feat: Update PaidPaymentOptionModel

--- src/main/java/it/gov/pagopa/debtposition/model/payments/response/PaidPaymentOptionModel.java
+++ src/main/java/it/gov/pagopa/debtposition/model/payments/response/PaidPaymentOptionModel.java
@@ -1,48 +1,13 @@
 package it.gov.pagopa.debtposition.model.payments.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import it.gov.pagopa.debtposition.model.enumeration.PaymentOptionStatus;
-import it.gov.pagopa.debtposition.model.pd.response.PaymentOptionMetadataModelResponse;
-import it.gov.pagopa.debtposition.model.pd.response.TransferModelResponse;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.io.Serializable;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 @Data
 @NoArgsConstructor
 @Schema(name = "PaymentsModelResponse")
-public class PaidPaymentOptionModel implements Serializable {
+public class PaidPaymentOptionModel extends PaymentOptionModelResponse {
 
-  /** generated serialVersionUID */
-  private static final long serialVersionUID = 4163071583836786527L;
-
-  private String nav;
-  private String iuv;
-  private String organizationFiscalCode;
   private String serviceType;
-  private long amount;
-  private String description;
-  private Boolean isPartialPayment;
-  private LocalDateTime dueDate;
-  private LocalDateTime retentionDate;
-  private LocalDateTime paymentDate;
-  private LocalDateTime reportingDate;
-  private LocalDateTime insertedDate;
-  private String paymentMethod;
-  private long fee;
-  private long notificationFee;
-  private String pspCompany;
-  private String idReceipt;
-  private String idFlowReporting;
-  private PaymentOptionStatus status;
-  private LocalDateTime lastUpdatedDate;
-  private LocalDateTime lastUpdatedDateNotificationFee;
-
-  private List<PaymentOptionMetadataModelResponse> paymentOptionMetadata = new ArrayList<>();
-
-  private List<TransferModelResponse> transfer = new ArrayList<>();
 }
