5c11fae04172339d437d154ac874f8f98ef97b14
Jacopo Carlini <jacopo.carlini@pagopa.it>
2025-08-07 16:40:36 +0200

    hotfix: set description by installment (#324)
    * hotfix: set description by installment
    
    * test: fix junit
    
    * Bump to version 0.14.11-1-hotfix-description [skip ci]
    
    * remove useless mapping of paymentdate
    
    ---------
    
    Co-authored-by: pagopa-github-bot <github-bot@pagopa.it>

--- src/main/java/it/gov/pagopa/debtposition/mapper/ConverterV3PPModelToEntity.java
+++ src/main/java/it/gov/pagopa/debtposition/mapper/ConverterV3PPModelToEntity.java
@@ -42,7 +42,6 @@ public class ConverterV3PPModelToEntity
     destination.setCompanyName(source.getCompanyName());
     destination.setOfficeName(source.getOfficeName());
     destination.setValidityDate(getValidityDate(source.getPaymentOption()));
-    destination.setPaymentDate(source.getPaymentDate());
     destination.setSwitchToExpired(getSwitchToExpired(source.getPaymentOption()));
 
     mapAndUpdateInstallments(source, destination);
@@ -87,6 +86,11 @@ public class ConverterV3PPModelToEntity
     List<PaymentOptionModelV3> sourceOptions = source.getPaymentOption();
     List<PaymentOption> optionsToRemove = new ArrayList<>(destination.getPaymentOption());
 
+    // Covered cases:
+    // - 1 Payment Option with [1:N] Installment (ie Opzione Rateale)
+    // - [1:N] Payment Option with 1 Installment (ie Opzione Multipla)
+    // - [1:N] Payment Option with 1 Installment and 1 Payment Option with [1:N] Installment (ie
+    // Opzione Unica + Opzione Rateale)
     if (sourceOptions != null) {
       for (PaymentOptionModelV3 sourceOption : sourceOptions) {
         for (InstallmentModel installment : sourceOption.getInstallments()) {
@@ -134,7 +138,7 @@ public class ConverterV3PPModelToEntity
     }
 
     destination.setAmount(sourceInstallment.getAmount());
-    destination.setDescription(source.getDescription());
+    destination.setDescription(sourceInstallment.getDescription());
     destination.setDueDate(sourceInstallment.getDueDate());
     destination.setFee(sourceInstallment.getFee());
     destination.setIsPartialPayment(source.getInstallments().size() > 1);
