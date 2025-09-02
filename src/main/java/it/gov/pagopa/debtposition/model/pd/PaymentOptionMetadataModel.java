b531fbc3c6afd1ede24ee7d8e6e4242b9258a704
alessio-acitelli <97529051+alessio-acitelli@users.noreply.github.com>
2025-09-02 15:05:51 +0200

    Fix: PIDM-858 IBAN and PostalIBAN according with SANP spec (#320)
    * [PIDM-858] IBAN check for according to SANP specifications
    
    * [PIDM-858] added junit test
    
    * Bump to version 0.14.9-PIDM-858-iban-according-SANP-spec [skip ci]
    
    * Bump to version 0.15.0-1-PIDM-858-iban-according-SANP-spec [skip ci]
    
    ---------
    
    Co-authored-by: pagopa-github-bot <github-bot@pagopa.it>
    Co-authored-by: Angelo Caporaso <56113767+cap-ang@users.noreply.github.com>

--- src/main/java/it/gov/pagopa/debtposition/model/pd/PaymentOptionMetadataModel.java
+++ src/main/java/it/gov/pagopa/debtposition/model/pd/PaymentOptionMetadataModel.java
@@ -2,8 +2,22 @@ package it.gov.pagopa.debtposition.model.pd;
 
 import java.io.Serializable;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+/**
+ * Model representing a metadata entry for a payment option.
+ * <p>
+ * This class is aligned with the PagoPA SANP specification
+ * at:
+ * https://github.com/pagopa/pagopa-api/blob/b70d47bfae96a4c8782ed3f78305126a0601ff46/xsd-common/sac-common-types-1.0.xsd#L104
+ * <p>
+ * According to the SANP:
+ * - Both {@code key} and {@code value} are required.
+ * - Maximum length: 140 characters.
+ */
 
 @Data
 @NoArgsConstructor
@@ -13,7 +27,10 @@ public class PaymentOptionMetadataModel implements Serializable {
   private static final long serialVersionUID = 4575041445781686511L;
 
   @NotBlank(message = "key is required")
+  @Size(max = 140, message = "key must not exceed 140 characters")
   private String key;
 
+  @NotBlank(message = "value is required")
+  @Size(max = 140, message = "value must not exceed 140 characters")
   private String value;
 }
