9c29b37fc910a88890fa51941df8000d49bf1b67
Angelo Caporaso <56113767+cap-ang@users.noreply.github.com>
2025-09-03 10:42:10 +0200

    [PIDM-902] refactoring: Reorder the modifiers

--- src/main/java/it/gov/pagopa/debtposition/config/SwaggerConfig.java
+++ src/main/java/it/gov/pagopa/debtposition/config/SwaggerConfig.java
@@ -27,11 +27,11 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SwaggerConfig {
 
-  private final static String INFO_API = "/info";
-  private final static String DEBT_POSITIONS_API = "/organizations/{organizationfiscalcode}/debtpositions";
-  private final static String DEBT_POSITIONS_BULK_API= "/organizations/{organizationfiscalcode}/debtpositions/bulk";
-  private final static String PAYMENTS_MARK_AS_PAID_API = "/organizations/{organizationfiscalcode}/paymentoptions/paids/{nav}";
-  private final static String DEBT_POSITION_API_BLOCK = "/organizations/{organizationfiscalcode}/debtpositions/**";
+  private static final String INFO_API = "/info";
+  private static final String DEBT_POSITIONS_API = "/organizations/{organizationfiscalcode}/debtpositions";
+  private static final String DEBT_POSITIONS_BULK_API= "/organizations/{organizationfiscalcode}/debtpositions/bulk";
+  private static final String PAYMENTS_MARK_AS_PAID_API = "/organizations/{organizationfiscalcode}/paymentoptions/paids/{nav}";
+  private static final String DEBT_POSITION_API_BLOCK = "/organizations/{organizationfiscalcode}/debtpositions/**";
 
   @Bean
   public OpenAPI customOpenAPI(
