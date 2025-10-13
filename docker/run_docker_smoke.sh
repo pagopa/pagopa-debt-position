9b33c396b007aa38db4c0c02bb1b3401903b71da
Francesco <cesareo.francesco@gmail.com>
2025-10-13 17:37:00 +0200

    fix pg

--- docker/run_docker_smoke.sh
+++ docker/run_docker_smoke.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-ENV="local"
+ENV="dev"
 
 #pip3 install yq
 
 image="service-local:latest"
-ENV="dev"
+
 export image=${image}
 
 FILE=.env
 if test -f "$FILE"; then
     rm .env
 fi
-
-#!/bin/bash
 
 config=$(yq -r '."microservice-chart".envConfig' ../helm/values-$ENV.yaml)
 IFS=$'\n'
@@ -31,16 +29,14 @@ for line in $(echo "$config" | yq -r '. | to_entries[] | select(.key) | "\(.key)
     echo "$key=$value" >> .env
 done
 
-
-keyvault=$(yq  -r '."microservice-chart".keyvault.name' ../helm/values-$ENV.yaml)
-secret=$(yq  -r '."microservice-chart".envSecret' ../helm/values-$ENV.yaml)
+keyvault=$(yq -r '."microservice-chart".keyvault.name' ../helm/values-$ENV.yaml)
+secret=$(yq -r '."microservice-chart".envSecret' ../helm/values-$ENV.yaml)
 for line in $(echo "$secret" | yq -r '. | to_entries[] | select(.key) | "\(.key)=\(.value)"'); do
   IFS='=' read -r -a array <<< "$line"
-  response=$(az keyvault secret show --vault-name $keyvault --name "${array[1]}")
+  response=$(az keyvault secret show --vault-name "$keyvault" --name "${array[1]}")
   response=$(echo "$response" | tr -d '\n')
   value=$(echo "$response" | yq -r '.value')
-  value=$(echo "$value" | sed 's/\$/\$\$/g')
-  value=$(echo "$value" | tr -d '\n')
+  value=$(echo "$value" | sed 's/\$/\$\$/g' | tr -d '\n')
   echo "${array[0]}=$value" >> .env
 done
 
