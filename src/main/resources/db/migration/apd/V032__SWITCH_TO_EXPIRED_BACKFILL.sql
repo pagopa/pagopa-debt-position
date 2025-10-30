4f12620a207e74e2b0f13d457e66f293e0690422
Angelo Caporaso <56113767+cap-ang@users.noreply.github.com>
2025-10-30 10:13:22 +0100

    chore: Add draft backfill steps V031-V033
    The backfill logic will change and be defined in batches.

---
+++ src/main/resources/db/migration/apd/V032__SWITCH_TO_EXPIRED_BACKFILL.sql
@@ -0,0 +1,21 @@
+-- 1 backfill from payment_position_id
+UPDATE payment_option po
+SET switch_to_expired = pp.switch_to_expired
+FROM payment_position pp
+WHERE po.switch_to_expired IS NULL and po.payment_position_id = pp.id;
+
+--No, because v1.0.0 is continuing to doesnt write to po start
+-- 2 for safety: any NULLs -> false
+-- UPDATE payment_option
+-- SET switch_to_expired = false
+-- WHERE switch_to_expired IS NULL;
+--No, because v1.0.0 is continuing to doesnt write to po end
+
+-- 3 NOT NULL + default
+ALTER TABLE payment_option
+  ALTER COLUMN switch_to_expired SET NOT NULL,
+  ALTER COLUMN switch_to_expired SET DEFAULT false;
+
+-- 4 drop from payment_position
+--ALTER TABLE payment_position
+--  DROP COLUMN IF EXISTS switch_to_expired;
