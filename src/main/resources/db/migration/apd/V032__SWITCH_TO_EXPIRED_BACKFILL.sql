dd08a5d26f3ae78ed9efb681deee4376d274928f
Angelo Caporaso <56113767+cap-ang@users.noreply.github.com>
2025-10-31 11:33:31 +0100

    fix: Add migration script for switch_to_expired and validity_date

--- src/main/resources/db/migration/apd/V032__SWITCH_TO_EXPIRED_BACKFILL.sql
+++ src/main/resources/db/migration/apd/V032__SWITCH_TO_EXPIRED_BACKFILL.sql
@@ -1,21 +1,57 @@
--- 1 backfill from payment_position_id
-UPDATE payment_option po
-SET switch_to_expired = pp.switch_to_expired
-FROM payment_position pp
-WHERE po.switch_to_expired IS NULL and po.payment_position_id = pp.id;
+DROP PROCEDURE IF EXISTS backfill_switch_to_expired();
 
---No, because v1.0.0 is continuing to doesnt write to po start
--- 2 for safety: any NULLs -> false
--- UPDATE payment_option
--- SET switch_to_expired = false
--- WHERE switch_to_expired IS NULL;
---No, because v1.0.0 is continuing to doesnt write to po end
+-- 1. Creation of the procedure
+CREATE OR REPLACE PROCEDURE apd.backfill_switch_to_expired(
+    batch_size INT DEFAULT 1000,
+    sleep_seconds NUMERIC DEFAULT 0.1
+)
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    updated_rows INT;
+    total_updated INT := 0;
+BEGIN
+    RAISE NOTICE 'Start ZDT backfill for payment_option.switch_to_expired (Batch size: %)', batch_size;
 
--- 3 NOT NULL + default
-ALTER TABLE payment_option
-  ALTER COLUMN switch_to_expired SET NOT NULL,
-  ALTER COLUMN switch_to_expired SET DEFAULT false;
+    LOOP
+        -- 2. Compute the batch to update
+        WITH batch_to_update AS (
+            SELECT po.id
+            FROM apd.payment_option po
+            WHERE po.switch_to_expired IS NULL
+            ORDER BY po.id
+            LIMIT batch_size
+            FOR UPDATE SKIP LOCKED
+        ),
+        update_result AS (
+            UPDATE apd.payment_option po
+            SET switch_to_expired = pp.switch_to_expired
+            FROM
+                apd.payment_position pp,
+                batch_to_update btu
+            WHERE
+                po.id = btu.id
+                AND po.payment_position_id = pp.id
+            RETURNING po.id
+        )
+        SELECT count(*) INTO updated_rows FROM update_result;
 
--- 4 drop from payment_position
---ALTER TABLE payment_position
---  DROP COLUMN IF EXISTS switch_to_expired;
+        total_updated := total_updated + updated_rows;
+
+        -- 3. Rows commit: releases the locks and makes the changes permanent for this batch.
+        COMMIT;
+
+        -- 4. Exits the loop if the last batch was empty
+        EXIT WHEN updated_rows = 0;
+
+        -- 5. Log your progress and wait
+        RAISE NOTICE '% rows updated (Total: %). Pause for %s...', updated_rows, total_updated, sleep_seconds;
+        PERFORM pg_sleep(sleep_seconds);
+
+    END LOOP;
+
+    RAISE NOTICE 'ZDT backfill completed. Total rows updated: %', total_updated;
+END $$;
+
+-- Performs backfill with batches of 10,000 rows
+CALL apd.backfill_switch_to_expired(10000);
