40f511e55fb5d3b5bae271c230650cce7f0df04b
Angelo Caporaso <56113767+cap-ang@users.noreply.github.com>
2025-11-10 09:40:40 +0100

    refactoring(database-migration): Move from procedures to functions (#363)
    * refactoring(database-migration): Move from procedures to functions
    
    To improve monitoring from ADF the procedures have been migrated to functions. Zero downtime is preserved because rows are selected, locked, and updated in a single statement. The loop is managed by the ADF pipeline.
    
    * refactoring(database-migration)
    
    * refactoring(database-migration): Add IF NOT EXISTS in index creation

--- src/main/resources/db/migration/apd/V034__VALIDITY_DATE_BACKFILL.sql
+++ src/main/resources/db/migration/apd/V034__VALIDITY_DATE_BACKFILL.sql
@@ -1,56 +1,40 @@
-DROP PROCEDURE IF EXISTS apd.backfill_validity_date(INT, NUMERIC);
+DROP FUNCTION IF EXISTS apd.fn_backfill_validity_date(INT);
 
--- 1. Creation of the procedure
-CREATE OR REPLACE PROCEDURE apd.backfill_validity_date(
-    batch_size INT DEFAULT 1000,
-    sleep_seconds NUMERIC DEFAULT 0.1
+CREATE OR REPLACE FUNCTION apd.fn_backfill_validity_date(
+    batch_size INT
 )
+RETURNS INT -- Returns the number of rows updated
 LANGUAGE plpgsql
 AS $$
 DECLARE
     updated_rows INT;
-    total_updated INT := 0;
 BEGIN
-    RAISE NOTICE 'Start ZDT backfill for payment_option.validity_date (Batch size: %)', batch_size;
+    -- 1. Find the batch, update and count
+    WITH batch_to_update AS (
+        SELECT po.id
+        FROM apd.payment_option po, apd.payment_position pp
+        WHERE po.payment_position_id = pp.id
+          AND po.validity_date IS NULL
+          AND pp.validity_date IS NOT NULL
+        ORDER BY po.id
+        LIMIT batch_size
+        FOR UPDATE SKIP LOCKED
+    ),
+    update_result AS (
+        UPDATE apd.payment_option po
+        SET validity_date = pp.validity_date
+        FROM
+            apd.payment_position pp,
+            batch_to_update btu
+        WHERE
+            po.id = btu.id
+            AND po.payment_position_id = pp.id
+        RETURNING 1 -- Returns a value for each updated row
+    )
+    -- 2. Counts the rows that have actually been updated
+    SELECT count(*) INTO updated_rows FROM update_result;
 
-    LOOP
-        -- 2. Compute the batch to update
-        WITH batch_to_update AS (
-			SELECT po.id
-			FROM apd.payment_option po, apd.payment_position pp
-			WHERE po.payment_position_id = pp.id
-			  AND po.validity_date IS NULL
-			  AND pp.validity_date IS NOT NULL
-			ORDER BY po.id
-			LIMIT batch_size
-			FOR UPDATE SKIP LOCKED
-        ),
-        update_result AS (
-            UPDATE apd.payment_option po
-            SET validity_date = pp.validity_date
-            FROM
-                apd.payment_position pp,
-                batch_to_update btu
-            WHERE
-                po.id = btu.id
-                AND po.payment_position_id = pp.id
-            RETURNING po.id
-        )
-        SELECT count(*) INTO updated_rows FROM update_result;
-
-        total_updated := total_updated + updated_rows;
-
-        -- 3. Rows commit: releases the locks and makes the changes permanent for this batch.
-        COMMIT;
-
-        -- 4. Exits the loop if the last batch was empty
-        EXIT WHEN updated_rows = 0;
-
-        -- 5. Log your progress and wait
-        RAISE NOTICE '% rows updated (Total: %). Pause for %s...', updated_rows, total_updated, sleep_seconds;
-        PERFORM pg_sleep(sleep_seconds);
-
-    END LOOP;
-
-    RAISE NOTICE 'ZDT backfill completed. Total rows updated: %', total_updated;
-END $$;
+    -- 3. Returns the count to the caller
+    RETURN updated_rows;
+END;
+$$;
---
+++ src/main/resources/db/migration/apd/V036__PARTIAL_OPT_BACKFILL_IDX.sql
@@ -0,0 +1,9 @@
+-- This migration creates 4 indexes to improve backfill performance;
+-- As the backfill progresses, rows (that meet the condition) become increasingly rare.
+-- To find the next batch, the database is forced to scan the po.id index, skipping millions of rows that have already been updated,
+-- just to find the few remaining NULL rows. This process becomes exponentially slower towards the end of the backfill.
+
+-- 2. Index for: fn_backfill_payment_plan_id_grouped_batch
+CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_payment_option_grouped_pending
+ON apd.payment_option (id)
+WHERE payment_plan_id IS NULL AND is_partial_payment IS TRUE;
