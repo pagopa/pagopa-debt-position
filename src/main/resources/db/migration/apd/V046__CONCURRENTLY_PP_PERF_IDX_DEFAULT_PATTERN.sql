68771f61a0a3c84459e8eace747806ffe2dbbd47
alessio-acitelli <alessio.acitelli@dgsspa.com>
2026-02-16 11:56:27 +0100

    [PIDM-1497] different script for index creation

--- src/main/resources/db/migration/apd/V046__CONCURRENTLY_PP_PERF_IDX_DEFAULT_PATTERN.sql
+++ src/main/resources/db/migration/apd/V046__CONCURRENTLY_PP_PERF_IDX_DEFAULT_PATTERN.sql
@@ -6,12 +6,3 @@
 CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_pp_org_insdate_notwisp
 ON apd.payment_position (organization_fiscal_code, inserted_date)
 WHERE service_type <> 'WISP';
-
--- Enables index-only scans for COUNT(*), avoiding full table or parallel sequential scans.
-CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_pp_org_notwisp
-ON apd.payment_position (organization_fiscal_code)
-WHERE service_type <> 'WISP';
-
--- Prevents expensive nested-loop scans in rare but very costly due_date queries by allowing fast index lookups per payment_position
-CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_po_org_ppid_duedate
-ON apd.payment_option (organization_fiscal_code, payment_position_id, due_date);
