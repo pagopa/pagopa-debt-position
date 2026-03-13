9a96c9f069f69d0f1ce45ddf942a74138865d62d
Angelo Caporaso <56113767+cap-ang@users.noreply.github.com>
2026-03-13 14:48:35 +0100

    refactoring

--- src/main/resources/db/migration/apd_archive/V10__Schedule_Cron_Cleanup_Staging.sql
+++
@@ -1,5 +0,0 @@
-SELECT cron.schedule(
-    'cleanup_staging_job',
-    '45 * * * *',                       -- At 45 minutes past the hour.
-    'CALL apd.cleanup_staging_table(24);' -- The SQL command to be executed, cleaning records dating back 24 hours.
-);
