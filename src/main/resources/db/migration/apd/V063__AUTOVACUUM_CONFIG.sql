763da5f8225286cd7a1c08e3e3cf37f6fe8b1d34
cap-ang <angelo.caporaso@pagopa.it>
2026-08-03 16:15:10 +0200

    feat: update autovacuum settings

--- src/main/resources/db/migration/apd/V063__AUTOVACUUM_CONFIG.sql
+++ src/main/resources/db/migration/apd/V063__AUTOVACUUM_CONFIG.sql
@@ -11,16 +11,16 @@
  */
 
 ALTER TABLE apd.payment_position SET (
-    autovacuum_vacuum_scale_factor = 0.01,
-    autovacuum_analyze_scale_factor = 0.01
+    autovacuum_vacuum_scale_factor = 0.02,
+    autovacuum_analyze_scale_factor = 0.02
 );
 
 ALTER TABLE apd.payment_option SET (
-    autovacuum_vacuum_scale_factor = 0.01,
-    autovacuum_analyze_scale_factor = 0.01
+    autovacuum_vacuum_scale_factor = 0.02,
+    autovacuum_analyze_scale_factor = 0.02
 );
 
 ALTER TABLE apd.transfer SET (
-    autovacuum_vacuum_scale_factor = 0.01,
-    autovacuum_analyze_scale_factor = 0.01
+    autovacuum_vacuum_scale_factor = 0.02,
+    autovacuum_analyze_scale_factor = 0.02
 );
